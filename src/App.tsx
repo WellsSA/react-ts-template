db491997c0ccfb1f6c5d52a9333b201b446c81be
Thierry Santos <thierry.santos@skyone.solutions>
2020-05-10 18:44:36 -0300

    chore(): configure commitlint

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Routes from 'routes';
+import GlobalStyle from 'theme/global';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
 
-function App() {
+import store from 'store';
+import { history } from 'services';
+import i18n from './i18n';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Provider store={store}>
+        <I18nextProvider i18n={i18n}>
+          <Router history={history}>
+            <GlobalStyle />
+            <Routes />
+          </Router>
+        </I18nextProvider>
+      </Provider>
+    </>
   );
-}
+};
 
 export default App;
